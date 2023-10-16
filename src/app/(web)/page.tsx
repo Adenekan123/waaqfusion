f71e82f00a0a1276d68f90725ddfc73f640a2ad8
Adenekan123 <mr.nekan123@gmail.com>
2023-10-16 18:28:58 +0100

    added founder video section

--- src/app/(web)/page.tsx
+++ src/app/(web)/page.tsx
@@ -259,6 +259,26 @@ const Home = () => {
           </div>
         </Container>
       </HeroWall>
+      <div className="ceo-video">
+        <Container styles="py-20 md:py-24 ">
+          <div className="md:flex items-center">
+            <div className="basis-7/12 text-center md:text-left">
+              <Stack gap="gap-4" direction="flex-col">
+                <Heading type={1} title="Why?" styles="text-orange-500"/>
+                <Heading type={1} title="What?" styles="text-violet-500" />
+                <Heading type={1} title="How?" styles="text-green-500" />
+              </Stack>
+              <p className="text-md md:text-xl mt-4 mb-12">Hear about our Approach from the founders themselves...</p>
+            </div>
+            <div className="basis-5/12 relative">
+              <FrameImage image="/images/why-what-how-desktop.webp" />
+              <div className="flex items-center justify-center play w-20 h-20 md:w-24 md:h-24 text-white rounded-full bg-gradient-to-r from-red-500 to-orange-500 absolute  top-2/4 -translate-y-2/4 md:-left-10 left-2/4 -translate-x-2/4 md:translate-x-0 z-10">
+              <FaPlay size={24} />
+            </div>
+            </div>
+          </div>
+        </Container>
+      </div>
     </>
   );
 };
--- src/components/ui/heading.tsx
+++ src/components/ui/heading.tsx
@@ -1,11 +1,21 @@
-import { IHeading } from '@/types'
-import React from 'react'
+import { IHeading } from "@/types";
+import React from "react";
 
-export const Heading = ({title,styles,type}:IHeading) => {
-  if(type === 3)    return <h2 className={`text-xl md:text-2xl font-bold ${styles}`}>{title}</h2>
+export const Heading = ({ title, styles, type }: IHeading) => {
+  if (type === 1)
+    return (
+      <h2
+        className={`text-5xl md:text-6xl xl:text-9xl leading-tight  font-extrabold ${styles}`}
+      >
+        {title}
+      </h2>
+    );
+  if (type === 3)
+    return (
+      <h2 className={`text-xl md:text-2xl font-bold ${styles}`}>{title}</h2>
+    );
 
   return (
     <h2 className={` text-3xl md:text-4xl font-bold ${styles}`}>{title}</h2>
-  )
-}
-
+  );
+};
