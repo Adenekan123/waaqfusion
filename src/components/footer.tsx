24eb122b94108126dbf302e11e02956f6aa71099
Adenekan123 <mr.nekan123@gmail.com>
2023-10-19 13:19:41 +0100

    footer update

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Container, Heading, HeroWall, Stack } from "./ui";
+import { Body, Container, Heading, HeroWall, Stack } from "./ui";
 import Link from "next/link";
 import Image from "next/image";
 import { FaFacebook } from "react-icons/fa6";
@@ -11,7 +11,7 @@ export const Footer = () => {
     <HeroWall>
       <Container styles="py-12 md:py-24 relative">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-12 md:gap-24  lg:gap-32 justify-between">
-          <Stack direction="flex-col" gap="gap-8">
+          <Stack direction="flex-col" gap="gap-4">
             <Link href={"#"} className="logo text-3xl">
               <Image
                 src={"/images/logo.png"}
@@ -22,19 +22,19 @@ export const Footer = () => {
               />
             </Link>
             <Heading
-              type={3}
+              type={4}
               title="Unlocking Creativity, Igniting Curiosity: Building Future Innovators."
               styles="text-white"
             />
 
-            <div className="flex gap-3">
-              <div className="w-12 h-12 rounded-full bg-white text-blue-600 shadow-lg flex items-center justify-center">
+            <div className="flex gap-3 pt-2">
+              <div className="w-11 h-11 rounded-full bg-white text-blue-600 shadow-lg flex items-center justify-center">
                 <FaFacebookF size={16} />
               </div>
-              <div className="w-12 h-12 rounded-full bg-white text-cyan-500 shadow-lg flex items-center justify-center">
+              <div className="w-11 h-11 rounded-full bg-white text-cyan-500 shadow-lg flex items-center justify-center">
                 <BsTwitter size={16} />
               </div>
-              <div className="w-12 h-12 rounded-full bg-white text-red-500 shadow-lg flex items-center justify-center">
+              <div className="w-11 h-11 rounded-full bg-white text-red-500 shadow-lg flex items-center justify-center">
                 <BsYoutube size={16} />
               </div>
             </div>
@@ -44,23 +44,31 @@ export const Footer = () => {
             <Stack direction="flex-col" gap="gap-4">
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Schools</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Schools
+                </Link>
               </div>
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Partners</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Partners
+                </Link>
               </div>
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Labs</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Labs
+                </Link>
               </div>
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Classroom Bundle</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Classroom Bundle
+                </Link>
               </div>
             </Stack>
           </div>
@@ -69,26 +77,59 @@ export const Footer = () => {
             <Stack direction="flex-col" gap="gap-4">
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Home</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Home
+                </Link>
               </div>
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Shop</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Shop
+                </Link>
               </div>
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Cart</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Cart
+                </Link>
               </div>
               <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
                 <BsFillCaretRightFill />
-                
-                <Link href={"#"} className="text-lg">Sign In</Link>
+
+                <Link href={"#"} className="text-lg">
+                  Sign In
+                </Link>
               </div>
             </Stack>
           </div>
+        </div>
+      </Container>
+      <Container styles="pb-3">
+        <div className="flex relative w-full">
+          <Body
+            title="Copyright © 2023 Waaqfusion, All rights reserved. Powered by Avishkaar."
+            styles="text-white text-center lg:text-left"
+            type={0.5}
+          />
+          {/* <Stack gap="gap-4" styles="">
+            <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
+              <BsFillCaretRightFill />
+
+              <Link href={"#"} className="text-sm">
+                Terms of use
+              </Link>
+            </div>
+            <div className="flex items-center pb-2 border-dashed border-orange-400 border-b text-orange-50 gap-3">
+              <BsFillCaretRightFill />
+
+              <Link href={"#"} className="text-sm">
+                Privacy policy
+              </Link>
+            </div>
+          </Stack> */}
         </div>
       </Container>
     </HeroWall>
