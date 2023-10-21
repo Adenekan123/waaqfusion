15536afde3889fdd4c1e0a3813404b0bcca1bf4b
Adenekan123 <mr.nekan123@gmail.com>
2023-10-21 19:59:04 +0100

    menu update

--- src/components/ui/stack.tsx
+++ src/components/ui/stack.tsx
@@ -2,6 +2,6 @@ import { IStack } from "@/types";
 import React from "react";
 
 export const Stack = ({ children, direction = "flex-col md:flex-row",gap="gap-12",styles }: IStack) => {
-  return <div className={`flex w-full h-full relative ${gap} ${direction} ${styles}`}>{children}</div>;
+  return <div className={`flex  h-full relative ${gap} ${direction} ${styles}`}>{children}</div>;
 };
 
