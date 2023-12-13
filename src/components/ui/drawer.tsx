5997cf4938b186e1f7d9188c39eb7cb562e47bcb
Adenekan123 <mr.nekan123@gmail.com>
2023-12-13 16:52:12 +0100

    cart position update

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -15,7 +15,7 @@ export const Drawer = ({
         id="drawer-example"
         className={`fixed top-0 ${
           left ? "left-0" : "right-0"
-        } z-50  h-screen overflow-y-auto transition-transform ${
+        } z-[2000]  h-screen overflow-y-auto transition-transform ${
           open ? "translate-x-0" : left ? "-translate-x-full" :"translate-x-full"
         }  bg-orange-50 shadow-2xl w-[100%] md:w-[45%] lg:w-[25%] `}
         tabIndex={-1}
